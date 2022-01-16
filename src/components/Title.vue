06d712f6648b969c538ed4dd4d829a4bf24edd80
krostyle <diegoo.albarran@gmail.com>
2022-01-16 04:11:52 -0300

    Se cambia origen de datos , se filtran datos y se muestran datos

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center max-w-2xl mx-auto">
+      <h2 class="text-3xl font-bold">{{title}}</h2>            
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    props: ['title', 'lastDate'],
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,35 @@
 <template>
   <main v-if="!loading">    
-    Show Data
+    <Title :title="title"/>
+    <div class="max-w-2xl mx-auto">
+      <div class="my-5 mx-auto p-4 max-w-full bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex justify-between items-center mb-4">
+          <h1 class="text-xl mx-auto font-bold leading-none text-gray-900 dark:text-white">Categorias</h1>                    
+        </div>
+        <div class="flow-root">
+          <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <li v-for="data in lastDataCovid" class="py-3 sm:py-4">
+                <div class="flex items-center space-x-4">                    
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            {{data.nombre}}
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            {{data.fecha}}
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                        {{data.casos}}
+                    </div>
+                </div>
+            </li>
+          </ul>  
+        </div>
+      </div>
+    </div>
   </main>
+
+
   <main v-else class="flex justify-center items-center">
     <div class="loader animate-spin ease-linear rounded-full border-8 border-t-8 border-gray-300 h-64 w-64">      
     </div>      
@@ -12,50 +40,57 @@
 // @ is an alias to /src
 import axios from 'axios'
 import csv from 'csvtojson'
+import Title from '@/components/Title'
 
 export default {
   name: 'Home',
   components: {    
+    Title
   },
   data(){
     return {
       loading:true,
-      title:'Totales Chile',
-      data:[],      
+      title:'Información de Covid Chile',
+      data:[],    
+      lastDataCovid:[]  
     }
   },
   methods:{
     async fetchCovidData(){
-      const {data} = await axios.get('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv')            
-      const json = await csv({
-        noheader:false,
-        output: "json",        
-      }).fromString(data)                        
-      return json
+      const {data} = await axios.get('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales.csv')             
+      const json = await csv().fromString(data);
+      const data_json=json.map(d=>{
+        const  {
+          ['Fecha']:nombre,
+          ...fechaCasos
+        } = d
+        const fechas=Object.keys(fechaCasos)
+        const casos=Object.values(fechaCasos)
+        return {
+          nombre,
+          fechas,
+          casos
+        }
+      })
+      return data_json
+    },
+    lastData(data_json){      
+      const lastData=data_json.map(data=>{
+        const lastDataNumber=(data.casos.length-1);
+        return {
+          nombre:data.nombre,
+          fecha:data.fechas[lastDataNumber],
+          casos:data.casos[lastDataNumber]
+        }
+      })
+      return lastData
     }
   },
   async created(){
     const data =await this.fetchCovidData()    
-    this.data=data.map(d=>{      
-      const {
-        ['Codigo comuna']:codigo_comuna,
-        ['Codigo region']:codigo_region,
-        ['Comuna']:comuna,
-        ['Poblacion']:poblacion,
-        ['Region']:region,
-        ['Tasa']:tasa,
-        ...fechas}=d
-      return {
-        codigo_comuna,
-        codigo_region,
-        comuna,
-        poblacion,
-        region,
-        tasa,
-        fechas
-      }      
-    })    
-    this.loading=false
+    this.data=data
+    this.lastDataCovid=this.lastData(data)
+    this.loading=false       
   }
   
 }
